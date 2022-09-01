d2c0f8353f226d62b9e978b0aadb9de3e87e6ca8
hodob <hodob76@gmail.com>
2022-09-01 09:59:07 +0900

    first commit

---
+++ src/test/java/com/ILuvJeju/ILuvJejuApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ILuvJeju;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ILuvJejuApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
